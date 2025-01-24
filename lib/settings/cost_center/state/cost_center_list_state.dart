5f44fb05f7e6dbb445ba3d15168b4d780315c5f7
abejarano <programador.angel@gmail.com>
2025-01-24 17:02:46 -0300

    feat: creacion del estado y almacenamiento del estado.

---
+++ lib/settings/cost_center/state/cost_center_list_state.dart
@@ -0,0 +1,20 @@
+import '../models/cost_center_model.dart';
+
+class CostCenterListState {
+  final bool makeRequest;
+  final List<CostCenterModel> costCenters;
+
+  CostCenterListState({required this.makeRequest, required this.costCenters});
+
+  factory CostCenterListState.empty() {
+    return CostCenterListState(makeRequest: false, costCenters: []);
+  }
+
+  CostCenterListState copyWith(
+      {bool? makeRequest, List<CostCenterModel>? costCenters}) {
+    return CostCenterListState(
+      makeRequest: makeRequest ?? this.makeRequest,
+      costCenters: costCenters ?? this.costCenters,
+    );
+  }
+}
