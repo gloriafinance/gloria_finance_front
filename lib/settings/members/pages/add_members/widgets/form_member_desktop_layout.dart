6289f644af8d7097ac76df4caf4d15b0846fdcc8
abejarano <programador.angel@gmail.com>
2025-04-06 15:23:48 -0300

    chore: implement form member functionality with state management and validation

--- lib/settings/members/pages/add_members/widgets/form_member_desktop_layout.dart
+++ lib/settings/members/pages/add_members/widgets/form_member_desktop_layout.dart
@@ -1,8 +1,10 @@
+import 'package:church_finance_bk/settings/members/pages/add_members/store/form_member_store.dart';
 import 'package:flutter/material.dart';
 
 import 'form_member_inputs.dart';
 
-Widget formMemberDesktopLayout(BuildContext context) {
+Widget formMemberDesktopLayout(
+    BuildContext context, FormMemberStore formStore) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -10,27 +12,35 @@ Widget formMemberDesktopLayout(BuildContext context) {
       Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(child: name()),
+          Expanded(child: name(formStore)),
           const SizedBox(width: 16),
-          Expanded(child: dni()),
+          Expanded(child: dni(formStore)),
         ],
       ),
       Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(child: birthdate(context)),
+          Expanded(child: birthdate(context, formStore)),
           const SizedBox(width: 16),
-          Expanded(child: phone()),
+          Expanded(child: phone(formStore)),
           const SizedBox(width: 16),
-          Expanded(child: email()),
+          Expanded(child: email(formStore)),
         ],
       ),
       Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(width: 200, child: conversionDate(context)),
+          SizedBox(width: 200, child: conversionDate(context, formStore)),
           const SizedBox(width: 16),
-          SizedBox(width: 200, child: baptismDate(context)),
+          SizedBox(width: 200, child: baptismDate(context, formStore)),
+          const SizedBox(width: 16),
+          SizedBox(
+            width: 216,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 25),
+              child: active(formStore),
+            ),
+          )
         ],
       ),
       const SizedBox(height: 16),
--- lib/settings/members/services/save_member_service.dart
+++ lib/settings/members/services/save_member_service.dart
@@ -1,3 +1,4 @@
+import 'package:church_finance_bk/auth/auth_persistence.dart';
 import 'package:church_finance_bk/core/app_http.dart';
 import 'package:dio/dio.dart';
 
@@ -5,6 +6,9 @@ class SaveMemberService extends AppHttp {
   SaveMemberService({super.tokenAPI});
 
   Future<void> saveMember(Map<String, dynamic> jsonForm) async {
+    final session = await AuthPersistence().restore();
+    tokenAPI = session.token;
+
     try {
       await http.post(
         '${await getUrlApi()}church/member',
