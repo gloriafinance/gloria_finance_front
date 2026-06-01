4827fccb4ada8a4e5ef93510a12e3cb1208f3500
abejarano <programador.angel@gmail.com>
2026-06-01 08:20:57 -0300

    fix(member_selector): update member selection to use member IDs and improve dropdown functionality

--- lib/features/erp/accounts_receivable/pages/register_accounts_receivable/widgets/member_selector.dart
+++ lib/features/erp/accounts_receivable/pages/register_accounts_receivable/widgets/member_selector.dart
@@ -20,48 +20,31 @@ class _MemberSelectorState extends State<MemberSelector> {
   String? _selectedMemberId;
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final memberAllStore = Provider.of<MemberAllStore>(context);
-
-    // Convertir los miembros a un formato compatible con el widget Dropdown
-    List<String> memberNames =
-        memberAllStore.getMembers().map((member) => member.name).toList();
+    final members = memberAllStore.getMembers();
+    final memberIds =
+        members.map((member) => member.memberId).toList(growable: false);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Dropdown(
           label: context.l10n.accountsReceivable_form_field_member,
-          items: memberNames,
+          items: memberIds,
           initialValue:
-              _selectedMemberId != null
-                  ? memberAllStore
-                      .getMembers()
-                      .firstWhere((m) => m.memberId == _selectedMemberId)
-                      .name
-                  : null,
-          onChanged: (selectedName) {
-            final member = memberAllStore.getMembers().firstWhere(
-              (m) => m.name == selectedName,
-              orElse:
-                  () => MemberModel(
-                    memberId: '',
-                    name: '',
-                    email: '',
-                    phone: '',
-                    dni: '',
-                    conversionDate: '',
-                    birthdate: '',
-                    isMinister: false,
-                    isTreasurer: false,
-                    active: true,
-                    address: '',
-                  ),
+              memberIds.contains(_selectedMemberId) ? _selectedMemberId : null,
+          itemLabelBuilder: (memberId) {
+            final member = members.cast<MemberModel?>().firstWhere(
+              (m) => m?.memberId == memberId,
+              orElse: () => null,
+            );
+
+            return member?.name ?? '';
+          },
+          onChanged: (selectedMemberId) {
+            final member = members.firstWhere(
+              (m) => m.memberId == selectedMemberId,
             );
 
             setState(() {
