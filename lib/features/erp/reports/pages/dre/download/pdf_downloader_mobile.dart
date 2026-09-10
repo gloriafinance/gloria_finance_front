41eb840785787a328c8626e68a9986f64d118d10
Angel Bejarano <programador.angel@gmail.com>
2026-09-10 17:54:43 -0300

    fix: remove broad Android storage permissions

--- lib/features/erp/reports/pages/dre/download/pdf_downloader_mobile.dart
+++ lib/features/erp/reports/pages/dre/download/pdf_downloader_mobile.dart
@@ -2,7 +2,7 @@
 
 import 'dart:io';
 
-import 'package:path_provider/path_provider.dart';
+import 'package:gloria_finance/core/download/report_storage_directory.dart';
 
 import 'pdf_downloader_base.dart';
 
@@ -10,22 +10,9 @@ class DREPdfDownloaderImpl implements DREPdfDownloader {
   @override
   Future<bool> savePdf(List<int> bytes, String fileName) async {
     try {
-      if (Platform.isAndroid) {
-        final downloadPath = Directory('/storage/emulated/0/Download');
-
-        if (!await downloadPath.exists()) {
-          await downloadPath.create(recursive: true);
-        }
-
-        final file = File('${downloadPath.path}/$fileName');
-        await file.writeAsBytes(bytes, flush: true);
-        return true;
-      }
-
-      final directory = await getApplicationDocumentsDirectory();
+      final directory = await getReportStorageDirectory();
       final file = File('${directory.path}/$fileName');
       await file.writeAsBytes(bytes, flush: true);
-
       return true;
     } catch (e) {
       print('Error al guardar el PDF localmente: $e');
